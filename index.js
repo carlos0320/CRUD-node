4725cd5219c6595ff3d69efa104f9a8149d9be29
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-05-31 17:58:15 -0500

    delete creado

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(bodyParser.json());
 
 
+app.delete('/post/:id', (req, res ) => {
+    const nuevoPost = post.filter( elemento => elemento.id !== Number(req.params.id))
+    
+    
+    res.status(200).json(nuevoPost);
+})
 
 app.listen(5003, () =>{
     console.log('escuchando en el puerto 5003')
