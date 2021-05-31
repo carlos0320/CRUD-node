240543217257655a7d3f0aab1f05a03f6893b8b9
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-05-31 16:45:58 -0500

    master creado

---
+++ data.js
@@ -0,0 +1,15 @@
+
+const post =[
+    {
+        titulo: "primer post",
+        autor: "carlos",
+        contenido: "mi primer post"
+    },
+    {
+        titulo: "segundo post",
+        autor: "Rafa",
+        contenido: "mi segundo post"
+    }
+]
+
+module.exports = post
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const post = require('./data')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+app.listen(5003, () =>{
+    console.log('escuchando en el puerto 5003')
+})
+
+
+
+
