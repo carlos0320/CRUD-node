154777f78898d2ea30e59d6db6fdb6ffbb5b3a87
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-05-31 18:41:26 -0500

    nuevo PATCH!

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.get('/post', (req, res) => {
     res.status(200).json(post)
 })
 
+app.patch('/post/:id', (req, res) => {
+
+    const nuevoPost= post.map( elemento => {
+         if( elemento.id == req.params.id){
+             return elemento = req.body
+         }else{
+             return elemento
+         }
+ 
+     })
+ 
+     res.status(200).json(nuevoPost)
+ })
+ 
 app.listen(5003, () =>{
     console.log('escuchando en el puerto 5003')
 })
